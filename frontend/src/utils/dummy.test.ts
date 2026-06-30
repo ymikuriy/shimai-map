dc11b2a82f45366cc8bc6ff004cdace0e0f58409
ymikuriy <rin.non.061825@gmail.com>
2026-06-30 14:24:15 +0900

    Delete frontend directory

--- frontend/src/utils/dummy.test.ts
+++
@@ -1,8 +0,0 @@
-import { describe, it, expect } from 'vitest';
-import { add } from './dummy';
-
-describe('dummy test', () => {
-  it('adds 1 + 2 to equal 3', () => {
-    expect(add(1, 2)).toBe(3);
-  });
-});
